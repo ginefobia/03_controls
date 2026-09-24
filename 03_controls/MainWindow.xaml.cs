581fdd8f3084f1a2526daf1eec48d89d9f0a601a
ginefobia <dmartin.smx@institutcampalans.net>
2026-09-24 16:20:01 +0200

    miprimeraapp

--- 03_controls/MainWindow.xaml.cs
+++ 03_controls/MainWindow.xaml.cs
@@ -23,6 +23,37 @@ namespace _03_controls
         public MainWindow()
         {
             InitializeComponent();
+            Grid PrimerGird = new Grid();
+            this.Content = PrimerGird;
+
+            Button bt = new Button();
+            WrapPanel wrapPanel = new WrapPanel();
+
+            TextBlock blocdetext1 = new TextBlock();
+            blocdetext1.Text = "Antropolophilia y";
+            wrapPanel.Children.Add(blocdetext1);
+
+            TextBlock blocdetext2 = new TextBlock();
+            blocdetext2.Text = " Blasfemia = ";
+            wrapPanel.Children.Add(blocdetext2);
+
+            TextBlock blocdetext3 = new TextBlock();
+            blocdetext3.Text = " Najib";
+            wrapPanel.Children.Add(blocdetext3);
+
+            bt.Content = wrapPanel;
+
+            PrimerGird.Children.Add(bt);
+
+            bt.Width = 200;
+            bt.Height = 100;
+
+            bt.Foreground=Brushes.Peru;
+            bt.Foreground = Brushes.DarkRed;
+            blocdetext1.Foreground = Brushes.DarkRed;
+            blocdetext2.Foreground = Brushes.DarkBlue;
+            blocdetext3.Foreground = Brushes.Magenta;
+
         }
     }
 }
